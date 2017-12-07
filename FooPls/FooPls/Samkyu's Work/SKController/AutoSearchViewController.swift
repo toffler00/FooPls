c980f93c378e363c0252340ba9989d007c649a99
Sam Kyu, Kang <sky4411v@gmail.com>
2017-12-07 16:10:01 +0900

    20171207 for repair git status

---
+++ FooPls/FooPls/Samkyu's Work/SKController/AutoSearchViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AutoSearchViewController.swift
+//  FooPls
+//
+//  Created by Samuel K on 2017. 12. 7..
+//  Copyright © 2017년 SONGYEE SHIN. All rights reserved.
+//
+
+import UIKit
+
+class AutoSearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
