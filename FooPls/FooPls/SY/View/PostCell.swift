5d00135d92f85a4d94b64e6500e9ce1f354c3072
ssy828 <828yss@gmail.com>
2017-12-11 02:32:54 +0900

    popview add ButtonAction popview UI

--- FooPls/FooPls/SY/View/PostCell.swift
+++ FooPls/FooPls/SY/View/PostCell.swift
@@ -1,12 +1,12 @@
 //
 //  PostCell.swift
 //  FinalPopView
-
-
 import UIKit
 
 class PostCell: UITableViewCell {
 
+    weak var postDelegate: PostCellDelegate?
+    
     @IBOutlet weak var postLB: UILabel!
     
     override func awakeFromNib() {
@@ -20,4 +20,8 @@ class PostCell: UITableViewCell {
         // Configure the view for the selected state
     }
     
+}
+// MARK: 프로토콜
+protocol PostCellDelegate: class {
+    func postCellData(_ cell: PostCell)
 }
