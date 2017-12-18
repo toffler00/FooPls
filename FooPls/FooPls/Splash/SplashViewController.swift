b2f67d4bb111a75ee6044feaecf5af7cec4e19ea
toffler00 <31604958+toffler00@users.noreply.github.com>
2017-12-18 17:52:17 +0900

    Revert "change postpage"

--- FooPls/FooPls/Splash/SplashViewController.swift
+++ FooPls/FooPls/Splash/SplashViewController.swift
@@ -18,10 +18,8 @@ class SplashViewController: UIViewController {
     // MARK: Life Cycle
     override func viewDidLoad() {        
         super.viewDidLoad()
-        try! Auth.auth().signOut()
-
-        FBSDKLoginManager().logOut()
-
+//        try! Auth.auth().signOut()
+//        FBSDKLoginManager().logOut()
         
         //MARK: - 애니메이션 동작, 동작 후 후행 클로저를 통해 현재 로그인이 되어 있는지 확인
         fooplsLoadingView.addSplashLoadingAnimation { [weak self] (action) in
