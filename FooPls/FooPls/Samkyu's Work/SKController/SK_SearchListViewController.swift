be8b8a726693d39cc6a037ed92e9fd89af2cd555
Sam Kyu, Kang <sky4411v@gmail.com>
2017-12-20 12:11:29 +0900

    20171220-1211

--- FooPls/FooPls/Samkyu's Work/SKController/SK_SearchListViewController.swift
+++ FooPls/FooPls/Samkyu's Work/SKController/SK_SearchListViewController.swift
@@ -14,10 +14,84 @@ import FirebaseDatabase
 import FirebaseStorage
 
 class SK_SearchListViewController: UIViewController {
+    
+    var resultsViewController: GMSAutocompleteResultsViewController?
+    var searchController: UISearchController?
+    var resultView:UITextView?
+    var zoom:Float = 14
+    
+    var adress:String = DataCenter.main.placeAddress
+    var placeName:String = DataCenter.main.placeName
+    var latitude:Double = DataCenter.main.latitude
+    var longitudue:Double = DataCenter.main.longitude
 
+    
+    @IBOutlet weak var googleMapView: GMSMapView!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        makeGoogleSearchBar()
+        startGoogleMap()
 
+    }
+    
+    func startGoogleMap(){
+        
+        
+        googleMapView.isMyLocationEnabled = true
+        googleMapView.settings.myLocationButton = true
+        
+        let camera = GMSCameraPosition.camera(withLatitude: latitude, longitude: longitudue, zoom: zoom)
+        self.googleMapView.camera = camera
+        
+        let marker = GMSMarker()
+        marker.map = googleMapView
+        marker.position = camera.target
+        marker.icon = UIImage(named: "GMarker")
+        marker.snippet = placeName
+        
+        
+    }
+    
+    func showSearchResult(lati: Double, longi:Double, placeName:String){
+        
+        googleMapView.clear()
+        
+        let camera = GMSCameraPosition.camera(withLatitude: lati, longitude: longi, zoom: zoom)
+        self.googleMapView.camera = camera
+        
+        let marker = GMSMarker()
+        marker.map = googleMapView
+        marker.position = camera.target
+        marker.snippet = placeName
+        marker.icon = UIImage(named: "GMarker")
+        
+        
+        
+    }
+    
+    func makeGoogleSearchBar(){
+        
+        resultsViewController = GMSAutocompleteResultsViewController()
+        resultsViewController?.delegate = self
+        
+        searchController = UISearchController(searchResultsController: resultsViewController)
+        searchController?.searchResultsUpdater = resultsViewController
+        
+        searchController?.searchBar.sizeToFit()
+        navigationItem.titleView = searchController?.searchBar
+        
+        definesPresentationContext = true
+        
+        searchController?.hidesNavigationBarDuringPresentation = false
+        self.navigationController?.navigationBar.barTintColor = UIColor(red: 248/255, green: 239/255, blue: 106/255, alpha: 1)
+        self.navigationController?.navigationBar.tintColor = .black
+        
     }
     
 }
@@ -36,5 +110,22 @@ extension SK_SearchListViewController : GMSAutocompleteResultsViewControllerDele
     }
     
     
+    
+}
+
+extension SK_SearchListViewController : UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        return cell
+        
+    }
     
 }
