e8ab0a87064ac98b744c31b7c577ae0429bb03ab
toffler00 <5chunwon@gmail.com>
2017-12-21 12:16:57 +0900

    push origin ilhan change datacenter, postingpage, detailpage

--- FooPls/FooPls/Ih_work/MainView/MainViewDetailpage/DetailPageView.swift
+++ FooPls/FooPls/Ih_work/MainView/MainViewDetailpage/DetailPageView.swift
@@ -1,6 +1,8 @@
 
 import UIKit
-
+import SDWebImage
+import Firebase
+import FirebaseStorage
 
 
 class DetailPageView: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout,     UICollectionViewDelegate, SendSelectedCellIntfo {
@@ -11,8 +13,8 @@ class DetailPageView: UIViewController, UICollectionViewDataSource, UICollection
     @IBOutlet weak var detailPageCollectionView: UICollectionView!
     
     
-    var postData : [PostModel] = []
-    
+    var postData : [PostModel] = DataCenter.main.mainVCpostsData
+    var cell : CustomCell!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,7 +24,13 @@ class DetailPageView: UIViewController, UICollectionViewDataSource, UICollection
         print("DetailViewPage \(postData.count)")
     }
 
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: self.view.frame.size.width, height: self.view.frame.size.height - 84)
+    }
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         print("타이밍이언제냐")
@@ -35,6 +43,25 @@ class DetailPageView: UIViewController, UICollectionViewDataSource, UICollection
         cell.storeNameLb.text = postData[indexPath.item].storeName
         cell.adressLb.text = postData[indexPath.item].storeAddress
         cell.showStoryLb.text = postData[indexPath.item].contentsText
+        cell.nickNameLb.text = postData[indexPath.item].nickName
+        cell.thoughtsLb.text = postData[indexPath.item].thoughts
+
+        
+        if let storeImgUrl = self.postData[indexPath.row].imageurl {
+            let url = URL(string: storeImgUrl)
+            cell.postImgView.sd_setImage(with: url!, completed: { (image, error, cacheType, url) in
+                if error != nil {
+                    print(error.debugDescription)
+                    cell.postImgView.image = #imageLiteral(resourceName: "noimage")
+                }
+                cell.postImgView.contentMode = .scaleAspectFill
+                cell.postImgView.clipsToBounds = true
+            })
+        }else {
+            cell.postImgView.image = #imageLiteral(resourceName: "noimage")
+        }
+        cell.thoughtsBackView.layer.borderColor = #colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)
+        cell.thoughtsBackView.layer.borderWidth = 1
         return cell
     }
     
